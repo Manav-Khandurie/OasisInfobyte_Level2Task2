994d8722d9a71cbb9f0b869056c59ee66752955b
Manav Khandurie <manavkhandurie@gmail.com>
2023-09-10 00:02:32 +0530

    updated work.sh

--- work.sh
+++ work.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-sudo su
-apt update -y
-apt install -y docker.io
-systemctl start docker
-systemctl enable docker
+
+sudo apt update -y
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
 docker build .
 docker run -itd -p 8080:80 $(docker images -q)
